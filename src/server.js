1d3733daa56fb42acea935ae613e7bcd0ee35b8d
Olivier Bieler <olivier.bieler@epfl.ch>
2023-10-23 14:31:28 +0200

    Add security config (#1)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 5050;
+
+app.use(cors());
 
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.json({ type: 'application/csp-report' }));
@@ -10,6 +13,9 @@ app.use(bodyParser.json({ type: 'application/csp-report' }));
 app.post('/csp-report', (req, res) => {
   const report = req.body['csp-report'];
   console.log('Received CSP violation report:', report);
+  res.header('Cross-Origin-Resource-Policy', 'cross-origin');
+  res.header('X-Content-Type-Options', 'nosniff');
+  res.header('Content-Type', 'text/plain');
   res.status(200).send('Report received successfully');
 });
 
